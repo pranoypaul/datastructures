4aba921f87ab58bb9f352c163c67cf62b9780ecf
Pranoy Paul <pranoypaul@gmail.com>
2021-02-24 19:59:26 +0530

    Added implementation of Hash Tables

--- Hash Tables/01-Implementation.py
+++ Hash Tables/01-Implementation.py
@@ -4,57 +4,74 @@ Created on Wed Feb 24 19:07:45 2021
 
 @author: pranoy
 """
-def display_hash(hashTable): 
-      
-    for i in range(len(hashTable)): 
-        print(i, end = " ") 
-          
-        for j in hashTable[i]: 
-            print("-->", end = " ") 
-            print(j, end = " ") 
-              
-        print() 
+
   
 """Defining hash table as a nester loop""" 
-HashTable = [[] for _ in range(10)] 
+
+class HashTable:
+    
+    def __init__(self, count):
+        
+        self.data=[[] for _ in range(count)]
+        self.count=count
   
-# Hashing Function to return  
-# key for every value. 
-def Hashing(keyvalue): 
-    return hash(keyvalue) % len(HashTable) 
-  
-  
-"""Inserting values into the hash table""" 
-def insert(Hashtable, keyvalue, value): 
+    # Hashing Function to return  
+    # key for every value. 
+    def Hashing(self,keyvalue): 
+        return hash(keyvalue) % self.count 
       
-    hash_key = Hashing(keyvalue) 
-    Hashtable[hash_key].append([keyvalue,value]) 
-
-"""Getting values from the has table"""
-def get(HashTable, keyvalue):
-    hash_key = Hashing(keyvalue)
-    try:
-        hash_arr = HashTable[hash_key]
-        for i in hash_arr:
-            if i[0]==keyvalue:
-                return i[1]
-        print('Invalid Key')
+      
+    """Inserting values into the hash table""" 
+    def insert(self,keyvalue, value): 
+          
+        hash_key = self.Hashing(keyvalue) 
+        self.data[hash_key].append([keyvalue,value]) 
+    
+    def display_hash(self): 
+      
+        for i in range(self.count): 
+            print(i, end = " ") 
+            for j in self.data[i]: 
+                print("-->", end = " ") 
+                print(j, end = " ") 
+                  
+            print() 
+        
+    """Getting values from the has table"""
+    def get(self,keyvalue):
+        hash_key = self.Hashing(keyvalue)
+        try:
+            hash_arr = self.data[hash_key]
+            for i in hash_arr:
+                if i[0]==keyvalue:
+                    return i[1]
+            print('Invalid Key')
+                    
+        except IndexError:
+            print('Invalid Key')
+        return
+    
+    def keys(self):
+        keyList = []
+        for hashArray in self.data:
+            for element in hashArray:
+                keyList.append(element[0])
+        return keyList
                 
-    except IndexError:
-        print('Invalid Key')
-    return
-  
+    
+
 # Driver Code 
-insert(HashTable, 10, 'Allahabad') 
-insert(HashTable, 25, 'Mumbai') 
-insert(HashTable, 20, 'Mathura') 
-insert(HashTable, 9, 'Delhi') 
-insert(HashTable, 21, 'Punjab') 
-insert(HashTable, 31, 'Noida') 
-insert(HashTable, 'str1', 'Noida') 
+myHashTable = HashTable(10)
+myHashTable.insert(10, 'Allahabad') 
+myHashTable.insert(25, 'Mumbai') 
+myHashTable.insert( 20, 'Mathura') 
+myHashTable.insert(9, 'Delhi') 
+myHashTable.insert(21, 'Punjab') 
+myHashTable.insert(31, 'Noida') 
+myHashTable.insert('str1', 'Noida') 
 
   
-display_hash (HashTable) 
+myHashTable.display_hash () 
 
-print(get(HashTable, 21))
-print(get(HashTable,'str1'))
+print(myHashTable.get(21))
+print(myHashTable.get('str1'))
